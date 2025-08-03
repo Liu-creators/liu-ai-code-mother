d4e27f0d2a02561ecb2114e5bd6ea2c31eef75ca
Liu <2162233967@qq.com>
2025-08-04 00:58:05 +0800

    refactor(core): 重构代码解析和保存逻辑
    - 新增 CodeParserExecutor 和 CodeFileSaverExecutor 类，统一处理不同类型的代码解析和保存
    - 实现 HtmlCodeParser 和 MultiFileCodeParser 类，分别处理 HTML 和多文件代码解析
    - 实现 CodeFileSaverTemplate 抽象类，提供代码保存的模板方法
    - 新增 HtmlCodeFileSaverTemplate 和 MultiFileCodeFileSaverTemplate 类，分别处理 HTML 和多文件代码保存
    - 重构 AiCodeGeneratorFacade 类，使用新的执行器类处理代码生成和保存逻辑
    -标记原有的 CodeFileSaver 和 CodeParser 类为 @Deprecated

---
+++ src/main/java/com/liu/liuaicodemother/core/saver/HtmlCodeFileSaverTemplate.java
@@ -0,0 +1,35 @@
+package com.liu.liuaicodemother.core.saver;
+
+import cn.hutool.core.util.StrUtil;
+import com.liu.liuaicodemother.ai.model.HtmlCodeResult;
+import com.liu.liuaicodemother.exception.BusinessException;
+import com.liu.liuaicodemother.exception.ErrorCode;
+import com.liu.liuaicodemother.model.enums.CodeGenTypeEnum;
+
+/**
+ * HTML代码文件保存器
+ *
+ * @author yupi
+ */
+public class HtmlCodeFileSaverTemplate extends CodeFileSaverTemplate<HtmlCodeResult> {
+
+    @Override
+    protected CodeGenTypeEnum getCodeType() {
+        return CodeGenTypeEnum.HTML;
+    }
+
+    @Override
+    protected void saveFiles(HtmlCodeResult result, String baseDirPath) {
+        // 保存 HTML 文件
+        writeToFile(baseDirPath, "index.html", result.getHtmlCode());
+    }
+
+    @Override
+    protected void validateInput(HtmlCodeResult result) {
+        super.validateInput(result);
+        // HTML 代码不能为空
+        if (StrUtil.isBlank(result.getHtmlCode())) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "HTML代码内容不能为空");
+        }
+    }
+}
